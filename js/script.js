4dfd418dd0e9b6b310b8a195ad16b26e02d1a3cb
painteyes <andreantoniocchipinti@gmail.com>
2021-11-29 08:54:28 +0100

    done

---
+++ js/script.js
@@ -0,0 +1,130 @@
+
+// -------
+// Snack 1
+// -------
+
+// Creare un array di oggetti
+    // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso
+
+const objectsArray = [
+    {
+        nome: 'Rockrider',
+        peso: 6,
+    },
+    {
+        nome: 'Discovery',
+        peso: 8,
+    },
+    {
+        nome: 'Alpina',
+        peso: 7,
+    },
+    {
+        nome: 'Riverside',
+        peso: 9,
+    }
+];
+
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
+
+    // Destructuring
+const [rockrider, discovery, alpina, riverside] = objectsArray;
+
+    // Extracting properties from the object
+let txt = "";
+
+for (let key in rockrider) {
+  txt += rockrider[key] + " ";
+}
+
+    // Appending the object to the DOM
+const mainContainer = document.getElementById('container');  
+
+mainContainer.innerHTML += `<div><h2>${txt}</h2></div>`;
+
+
+// -------
+// Snack2
+// -------
+
+// Creare un array di oggetti di squadre di calcio. 
+    // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+        // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+
+const teams = [
+    {
+        nome: 'Milan',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Napoli',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Inter',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Atalanta',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Fiorentina',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Cagliari',
+        punti: 0,
+        falli: 0,
+    },
+    {
+        nome: 'Sampdoria',
+        punti: 0,
+        falli: 0,
+    } 
+];
+
+// Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+    // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const teamsCopy = [];
+
+for (let i= 0; i < teams.length; i++) {
+
+    let thisTeam = teams[i];
+
+    thisTeam.punti = getRandomInt(0, 80);
+    thisTeam.falli = getRandomInt(0, 60);
+
+    // Destructuring
+    const {nome, falli} = thisTeam;
+
+    // Creating new object -> element of array
+    const teamsProperties = {
+        nome,
+        falli
+    };
+    
+    // Adding the object to the array
+    teamsCopy.push(teamsProperties);
+}
+
+console.log(teamsCopy);
+
+
+// ---------
+// Functions
+// ---------
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+
